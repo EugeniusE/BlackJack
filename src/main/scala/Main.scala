fe5ba257bb31c397ee8450fb4d62ac63635bcd2f
erbeu <erbeugen@gmail.com>
2024-04-08 08:15:44 +0200

    removed not working fx file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,33 @@
-@main def hello(): Unit =
-  def msg = "I was compiled by Scala 3. :)"
-  val app = new ScalaFXHelloWorld()
-  app.start()
-  println("Hello world!")
-  println(msg)
+// @main def hello(): Unit =
+//   println("Hello world!")
+//   println(msg)
 
  
-// object PlayingField {
-//   // Define the dimensions of the playing field
-//   val width = 10
-//   val height = 5
+object PlayingField {
+  // Define the dimensions of the playing field
+  val width = 10
+  val height = 5
   
-//   // Method to generate the string representation of the playing field
-//   def generateField(): String = {
-//     val builder = new StringBuilder()
+  // Method to generate the string representation of the playing field
+  def generateField(): String = {
+    val builder = new StringBuilder()
     
-//     for (y <- 0 until height) {
-//       for (x <- 0 until width) {
+    for (y <- 0 until height) {
+      for (x <- 0 until width) {
         
-//         builder.append('.')
-//       }
+        builder.append('.')
+      }
       
-//       builder.append('\n')
-//     }
+      builder.append('\n')
+    }
     
     
-//     builder.toString()
-//   }
-//   //main calls generate field
-//   def main(args: Array[String]): Unit = {
-//     val fieldString = generateField()
-//     println(fieldString)
-//   }
-//}
+    builder.toString()
+  }
+  //main calls generate field
+  def main(args: Array[String]): Unit = {
+    val fieldString = generateField()
+    println(fieldString)
+  }
+}
 
--- src/main/scala/TestSheet.worksheet.sc
+++ src/main/scala/TestSheet.worksheet.sc
@@ -1,3 +1,4 @@
 val List = (1,2,3,8,5)
 val Y = 356
-val Suits = ("Hearts","Diamonds","Spades","Clubs") 
+val Suits = ("Hearts","Diamonds","Spades","Clubs")
+val S = "Hey Leute"
