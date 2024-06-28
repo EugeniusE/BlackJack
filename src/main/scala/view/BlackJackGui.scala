7eb7928a2b6d53cc32534fb01a763e125c3a44d5
EugeniusE <erbeugen@gmail.com>
2024-06-28 14:33:47 +0200

    fixed laptop but by resizesing to scene after scene switch

--- src/main/scala/view/BlackJackGui.scala
+++ src/main/scala/view/BlackJackGui.scala
@@ -22,27 +22,46 @@ class GUI(controller: ControllerInterface) extends JFXApp3 with util.Observer {
   private var gameScene: GameScene = uninitialized
   private var resultScene: ResultScene = uninitialized
 
-  private var windowWidth = 1800.0
+  private var windowWidth = 1700.0
   private var windowHeight = 900.0
 
   private val minWindowWidth = 500.0
   private val minWindowHeight = 300.0
-  private val cursorImage = new Image(new FileInputStream(s"src/main/scala/resources/Chip.png"))
+  private val cursorImage = new Image(
+    new FileInputStream(s"src/main/scala/resources/Chip.png")
+  )
 
   override def start(): Unit = {
+    windowWidth = Screen.primary.bounds.width * 0.85
+    windowHeight = Screen.primary.bounds.height * 0.85
 
-    // val screenWidth = Screen.primary.bounds.width
-    // val screenHeight = Screen.primary.bounds.height
-
-    // // Calculate preferred window dimensions (adjust as needed)
-    //  windowWidth = screenWidth * 0.8 // 80% of screen width
-    //  windowHeight = screenHeight * 0.8 // 80% of screen height
-    preGameScene = PreGameScene(controller, windowWidth, windowHeight, () => stage.setScene(gameScene))
-    gameScene = GameScene(controller, windowWidth, windowHeight, () => stage.setScene(resultScene))
+    preGameScene = PreGameScene(
+      controller,
+      windowWidth,
+      windowHeight,
+      () => {
+        stage.setScene(gameScene)
+        stage.sizeToScene()
+      }
+    )
+    gameScene = GameScene(
+      controller,
+      windowWidth,
+      windowHeight,
+      () => {
+        stage.setScene(resultScene)
+        stage.sizeToScene()
+      }
+    )
     gameScene.setCursor(new ImageCursor(cursorImage))
-    resultScene = ResultScene(windowWidth, windowHeight, () => stage.setScene(preGameScene))
-
-
+    resultScene = ResultScene(
+      windowWidth,
+      windowHeight,
+      () => {
+        stage.setScene(preGameScene)
+        stage.sizeToScene()
+      }
+    )
 
     // val iconImage = new Image(getClass.getResourceAsStream("/Users/simonkann/Documents/Se/BlackJack/src/main/scala/resources/icon.png"))
     stage = new JFXApp3.PrimaryStage {
@@ -56,10 +75,11 @@ class GUI(controller: ControllerInterface) extends JFXApp3 with util.Observer {
         System.exit(0)
       }
 
-      
       val primaryScreenBounds = Screen.primary.bounds
-      x = primaryScreenBounds.getMinX + (primaryScreenBounds.getWidth - windowWidth) / 2
-      y = primaryScreenBounds.getMinY + (primaryScreenBounds.getHeight - windowHeight) / 2
+      x =
+        primaryScreenBounds.getMinX + (primaryScreenBounds.getWidth - windowWidth) / 2
+      y =
+        primaryScreenBounds.getMinY + (primaryScreenBounds.getHeight - windowHeight) / 2
     }
     controller.newGame()
   }
