22474cf2693f4d2ad4e2711d2b7b6bc1983fa07f
Simon Kanngiesser <simon.kanngiesser@gmail.com>
2024-07-01 16:34:01 +0200

    updated TestSpec

--- src/test/scala/ControllerSpec.scala
+++ src/test/scala/ControllerSpec.scala
@@ -1,14 +1,16 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 import scala.collection.mutable.ArrayBuffer
-import util.Decks.{Card,Rank,Suite}
+import util.Decks.{Card, Rank, Suite, Deck}
 import util._
 import control._
 import model.Player
+
 class ControllerSpec extends AnyWordSpec {
-  val game = new GameType(new StandardEvaluationStrategy,FactoryType.StandartDeck,new Player(500,"Spieler1"))
+  val game = new GameType(new StandardEvaluationStrategy, FactoryType.StandartDeck, new Player(500, "Spieler1"))
+
   "A Controller" when {
-    
+
     "a new game is started" should {
       "initialize a new game with non-empty deck and hands" in {
         val controller = new Controller(game)
@@ -24,16 +26,16 @@ class ControllerSpec extends AnyWordSpec {
       }
     }
 
-    "a new round is started each players should " should {
-      "initialize a new round " in {
-      val controller = new Controller(game)
-      controller.newGame()
-      controller.nextRound()
+    "a new round is started each player" should {
+      "initialize a new round" in {
+        val controller = new Controller(game)
+        controller.newGame()
+        controller.nextRound()
         controller.getDeck().size should be > 0
         controller.getPlayerHand() should not be empty
         controller.getDealerHand() should not be empty
       }
-    } 
+    }
 
     "hitting" should {
       "add a card to the player's hand" in {
@@ -48,12 +50,11 @@ class ControllerSpec extends AnyWordSpec {
       "return dealer win" in {
         val controller = new Controller(game)
         controller.newGame()
-        controller.addPlayerHand(Card(Rank.Ten,Suite.Spade))
-        controller.addPlayerHand(Card(Rank.Queen,Suite.Spade))
-        controller.addPlayerHand(Card(Rank.Jack,Suite.Spade))
+        controller.addPlayerHand(Card(Rank.Ten, Suite.Spade))
+        controller.addPlayerHand(Card(Rank.Queen, Suite.Spade))
+        controller.addPlayerHand(Card(Rank.Jack, Suite.Spade))
         controller.hit()
         controller.getOutcome() shouldEqual Ergebnis.DealerWin
-        
       }
     }
 
@@ -64,7 +65,6 @@ class ControllerSpec extends AnyWordSpec {
         controller.addPlayerHand(new Card(Rank.Nine, Suite.Heart))
         controller.addDealerHand(new Card(Rank.Ten, Suite.Club))
         controller.addDealerHand(new Card(Rank.Ten, Suite.Club))
-        controller.addDealerHand(new Card(Rank.Five, Suite.Club))
 
         controller.stand()
         controller.getOutcome() shouldEqual Ergebnis.PlayerWin
@@ -97,41 +97,103 @@ class ControllerSpec extends AnyWordSpec {
       }
     }
 
-    "Dealer hand " should{
-        "Be empty " in {
-            val controller = new Controller(game)
-            controller.clearDealerhand()
-            controller.getDealerHand().size shouldEqual 0
-        }
-        "get Cards" in {
-          val controller = new Controller(game)
-          controller.addPlayerHand((new Card(Rank.Ace, Suite.Spade)))
-          controller.stand()
-          controller.getDealerHand().size should  be > 1
-        }
-    }
-
-    "A new Deck is made" should {
-
-    "apper when drawing 52 cards" in {
-        val c = new Controller(game)
-            for(_<-0 to 52){
-                c.drawNewCard()
-            }
-        c.getDeck().size shouldEqual 51
-
+    "Dealer hand" should {
+      "be empty" in {
+        val controller = new Controller(game)
+        controller.clearDealerhand()
+        controller.getDealerHand().size shouldEqual 0
+      }
+      "get Cards" in {
+        val controller = new Controller(game)
+        controller.addPlayerHand((new Card(Rank.Ace, Suite.Spade)))
+        controller.stand()
+        controller.getDealerHand().size should be > 1
       }
     }
 
-    "A high stakes Game " should{
+    "A new Deck is made" should {
+      "appear when drawing 52 cards" in {
+        val c = new Controller(game)
+        for (_ <- 0 to 52) {
+          c.drawNewCard()
+        }
+        c.getDeck().size shouldEqual 51
+      }
+    }
+
+    "A high stakes Game" should {
       val eval = new HighStakes
-      val hand= ArrayBuffer(new Card(Rank.Ace, Suite.Spade), new Card(Rank.Ace, Suite.Spade))
+      val hand = ArrayBuffer(new Card(Rank.Ace, Suite.Spade), new Card(Rank.Ace, Suite.Spade))
       "evaluate 2 aces as 22" in {
         eval.evaluateHand(hand) shouldEqual 22
       }
     }
 
+    "betting" should {
+      "set and clear bet correctly" in {
+        val controller = new Controller(game)
+        controller.newGame()
+        controller.betCommand(100)
+        controller.getBet() shouldEqual 100
+        controller.clearBet()
+        controller.getBet() shouldEqual 0
+      }
+    }
 
+    "saving and loading game" should {
+      "save and load the game state correctly using JSONFileIO" in {
+        val controller = new Controller(game)
+        val jsonFileIO = new JSONFileIO(game)
+        controller.newGame()
+        controller.addPlayerHand(new Card(Rank.Ten, Suite.Spade))
+        jsonFileIO.save(controller.table)
 
+        val newController = new Controller(game)
+        newController.table = jsonFileIO.load
+
+        newController.getPlayerHand() should contain (new Card(Rank.Ten, Suite.Spade))
+      }
+
+      "save and load the game state correctly using XMLFileIO" in {
+        val controller = new Controller(game)
+        val xmlFileIO = new XMLFileIO(game)
+        controller.newGame()
+        controller.addPlayerHand(new Card(Rank.Ten, Suite.Spade))
+        xmlFileIO.save(controller.table)
+
+        val newController = new Controller(game)
+        newController.table = xmlFileIO.load
+
+        newController.getPlayerHand() should contain (new Card(Rank.Ten, Suite.Spade))
+      }
+    }
+
+    "undoing and redoing commands" should {
+      "undo the last command correctly" in {
+        val controller = new Controller(game)
+        controller.newGame()
+        val initialHandSize = controller.getPlayerHand().size
+
+        controller.hit()
+        controller.getPlayerHand().size shouldEqual initialHandSize + 1
+
+        controller.undoLastCommand()
+        controller.getPlayerHand().size shouldEqual initialHandSize
+      }
+      "redo the last undone command correctly" in {
+        val controller = new Controller(game)
+        controller.newGame()
+        val initialHandSize = controller.getPlayerHand().size
+
+        controller.hit()
+        controller.getPlayerHand().size shouldEqual initialHandSize + 1
+
+        controller.undoLastCommand()
+        controller.getPlayerHand().size shouldEqual initialHandSize
+
+        controller.redoLastUndoneCommand()
+        controller.getPlayerHand().size shouldEqual initialHandSize + 1
+      }
+    }
   }
 }
